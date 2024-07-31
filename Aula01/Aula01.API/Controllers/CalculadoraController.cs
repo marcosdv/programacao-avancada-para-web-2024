6e5a6ca9d8a27f29496dc91c46ea6a5ecf330855
Marcos Dias Vendramini <marcosdiasvendramini@yahoo.com.br>
2024-07-31 00:21:52 -0300

    Aula01 - 30-07-2024

---
+++ Aula01/Aula01.API/Controllers/CalculadoraController.cs
@@ -0,0 +1,53 @@
+﻿using Aula01.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aula01.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class CalculadoraController : ControllerBase
+    {
+        [HttpGet("Somar/{n1}/{n2}")]
+        public float Somar(float n1, float n2)
+        {
+            return n1 + n2;
+        }
+
+        [HttpPost("Subtrair/{n1}/{n2}")]
+        public float Subtrair(float n1, float n2)
+        {
+            return n1 - n2;
+        }
+
+        [HttpPut("Multiplicar")]
+        public float Multiplicar(Valores valores)
+        {
+            return valores.N1 * valores.N2;
+        }
+
+        [HttpDelete("Dividir")]
+        public IActionResult Dividir(Valores valores)
+        {
+            try
+            {
+                if (valores.N1 == 0 && valores.N2 == 0)
+                {
+                    return NoContent();
+                }
+
+                if (valores.N2 == 0)
+                {
+                    return BadRequest("N2 nao pode ser zero");
+                }
+
+                return Ok(valores.N1 / valores.N2);
+            }
+            catch (Exception ex)
+            {
+                
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
---
+++ Aula01/Aula01.API/Models/Valores.cs
@@ -0,0 +1,10 @@
+﻿namespace Aula01.API.Models
+{
+    public class Valores
+    {
+        public float N1 { get; set; }
+        public float N2 { get; set; }
+
+        public bool N1MaiorN2 { get => N1 > N2; }
+    }
+}
