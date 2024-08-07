72716cd8af5a06483fa2ed4fe933feb3d08e3494
Marcos Dias Vendramini <marcosdiasvendramini@yahoo.com.br>
2024-08-06 22:29:33 -0300

    Aula02 - 06-08-2024

---
+++ Aula02/Aula02/Controllers/OperadoraController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace Aula02.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class OperadoraController : ControllerBase
+{
+    SqlConnection conexao;
+
+    public OperadoraController()
+    {
+        conexao = new SqlConnection(
+@"Server=WINMACDELL-PRFO\SQLEXPRESS;
+Database=AgendaFoneDb;
+Trusted_Connection=True;"
+        );
+
+        conexao.Open();
+    }
+
+    [HttpGet]
+    public IList<string> BuscarTodas()
+    {
+        string sql = "SELECT * FROM TbOperadora";
+        var command = new SqlCommand(sql, conexao);
+
+        SqlDataReader reader = command.ExecuteReader();
+
+        var retorno = new List<string>();
+
+        while (reader.Read())
+        {
+            //string nome = reader.GetString(1);
+            string nome = reader["OpeNome"].ToString() ?? "";
+            
+            retorno.Add(nome);
+        }
+
+        return retorno;
+    }
+}
