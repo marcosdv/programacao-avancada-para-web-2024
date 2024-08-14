665c357f9fdd9b0b7d4fccb5bcb6f42eb5858078
Marcos Dias Vendramini <marcosdiasvendramini@yahoo.com.br>
2024-08-13 22:16:24 -0300

    Aula03 - 13-08-2024

---
+++ Aula02/Aula02/Controllers/OperadoraController.cs
@@ -0,0 +1,51 @@
+﻿using Aula02.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace Aula02.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class OperadoraController : ControllerBase
+{
+    OperadoraRepository repository;
+
+    public OperadoraController()
+    {
+        repository = new OperadoraRepository();
+    }
+
+    [HttpGet]
+    public IActionResult BuscarTodas()
+    {
+        try
+        {
+            var lista = repository.BuscarTodas();
+            if (lista.Any()) //if (lista.Count > 0)
+            {
+                return Ok(lista); //StatusCode(200, lista);
+            }
+            else
+            {
+                return NoContent(); //StatusCode(204);
+            }
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, ex.Message);
+        }
+    }
+
+    [HttpPost]
+    public IActionResult Adicionar(string nome)
+    {
+        try
+        {
+            return Ok(repository.Adicionar(nome));
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, ex.Message);
+        }
+    }
+}
---
+++ Aula02/Aula02/Repositories/OperadoraRepository.cs
@@ -0,0 +1,65 @@
+﻿using System.Data.SqlClient;
+
+namespace Aula02.Repositories
+{
+    public class OperadoraRepository
+    {
+        SqlConnection conexao;
+
+        public OperadoraRepository()
+        {
+            conexao = new SqlConnection(
+                @"Server=LABORATORIO-01\SQLEXPRESS;
+                  Database=AgendaFoneDb;
+                  Trusted_Connection=True;"
+            );
+        }
+
+        public IList<string> BuscarTodas()
+        {
+            try
+            {
+                conexao.Open();
+
+                string sql = "SELECT * FROM TbOperadora";
+                using SqlCommand command = new SqlCommand(sql, conexao);
+                using SqlDataReader reader = command.ExecuteReader();
+                var retorno = new List<string>();
+
+                while (reader.Read())
+                {
+                    retorno.Add(reader["OpeNome"].ToString() ?? "");
+                }
+
+                return retorno;
+            }
+            finally
+            {
+                conexao.Close();
+            }
+        }
+
+        public bool Adicionar(string nome)
+        {
+            try
+            {
+                conexao.Open();
+
+                string sql = "INSERT INTO TbOperadora (OpeNome) VALUES (@OpeNome)";
+                using SqlCommand command = new SqlCommand(sql, conexao);
+                command.Parameters.AddWithValue("@OpeNome", nome);
+                command.ExecuteNonQuery();
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+            finally
+            {
+                conexao.Close();
+            }
+        }
+    }
+}
