d19b468e1e28e251690101c4075a884d02e9b18f
Marcos Dias Vendramini <marcosdiasvendramini@yahoo.com.br>
2024-08-20 22:31:04 -0300

    Aula04 - 20-08-2024

---
+++ Aula02/Aula02/Repositories/PessoaRepository.cs
@@ -0,0 +1,25 @@
+﻿using Aula02.Models;
+using Dapper;
+using System.Data.SqlClient;
+
+namespace Aula02.Repositories
+{
+    public class PessoaRepository
+    {
+        SqlConnection conexao;
+
+        public PessoaRepository()
+        {
+            conexao = new SqlConnection(
+                @"Server=LABORATORIO-01\SQLEXPRESS;
+                  Database=AgendaFoneDb;
+                  Trusted_Connection=True;"
+            );
+        }
+
+        public IEnumerable<Pessoa> BuscarTodas()
+        {
+            return conexao.Query<Pessoa>("SELECT * FROM TbPessoa");
+        }
+    }
+}
