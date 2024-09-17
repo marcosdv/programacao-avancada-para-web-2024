b7e8960b866e30d29e480a5eed7fc1e21d0fe00d
Marcos Dias Vendramini <marcosdiasvendramini@yahoo.com.br>
2024-09-16 21:54:23 -0300

    Aula07 - 10-09-2024

---
+++ Aula02/Aula02/Controllers/TelefoneController.cs
@@ -0,0 +1,57 @@
+﻿using Aula02.Models;
+using Aula02.Repositories.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aula02.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TelefoneController : ControllerBase
+    {
+        private readonly ITelefoneRepository _telefoneRepository;
+
+        public TelefoneController(ITelefoneRepository telefoneRepository)
+        {
+            _telefoneRepository = telefoneRepository;
+        }
+
+        //[HttpGet]
+        //public IActionResult BuscarTodos() => Ok(_telefoneRepository.BuscarTodos());
+
+        [HttpGet]
+        public IActionResult BuscarTodos()
+        {
+            var telefones = _telefoneRepository.BuscarTodos();
+            return Ok(telefones);
+        }
+
+        [HttpPost]
+        public IActionResult Adicionar(Telefone telefone)
+        {
+            var result = _telefoneRepository.Adicionar(telefone);
+            return Ok(result);
+        }
+
+        [HttpPut]
+        public IActionResult Alterar(Telefone telefone)
+        {
+            var result = _telefoneRepository.Alterar(telefone);
+            return Ok(result);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Excluir(int id)
+        {
+            var result = _telefoneRepository.Excluir(id);
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult BuscarPorId(int id)
+        {
+            var result = _telefoneRepository.BuscarPorId(id);
+            return Ok(result);
+        }
+    }
+}
---
+++ Aula02/Aula02/Repositories/Interfaces/IOperadoraRepository.cs
@@ -0,0 +1,14 @@
+﻿using Aula02.Models;
+
+namespace Aula02.Repositories.Interfaces
+{
+    public interface IOperadoraRepository
+    {
+        public IList<Operadora> BuscarTodas();
+        public Operadora? BuscarPorId(int codigo);
+
+        public bool Adicionar(Operadora operadora);
+        public bool Alterar(Operadora operadora);
+        public bool Apagar(int codigo);
+    }
+}
