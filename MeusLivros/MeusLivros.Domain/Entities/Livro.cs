c07c1daf29b81784a5ea43f7b231366661c9feb6
Marcos Dias Vendramini <marcosdiasvendramini@yahoo.com.br>
2024-10-01 22:12:23 -0300

    Aula09 - 01-10-2024

---
+++ MeusLivros/MeusLivros.Domain/Entities/Livro.cs
@@ -0,0 +1,31 @@
+﻿namespace MeusLivros.Domain.Entities
+{
+    public class Livro : Entity
+    {
+        #region | Propriedades |
+        public string Nome { get; set; }
+        public DateTime Lancamento { get; set; }
+        public int EditoraId { get; set; }
+        public Editora Editora { get; set; }
+        #endregion
+
+        #region | Contrutores |
+
+        public Livro(string nome, DateTime lancamento, int editoraId)
+        {
+            Nome = nome;
+            Lancamento = lancamento;
+            EditoraId = editoraId;
+        }
+
+        public Livro(int id, string nome, DateTime lancamento, int editoraId)
+        {
+            Id = id;
+            Nome = nome;
+            Lancamento = lancamento;
+            EditoraId = editoraId;
+        }
+
+        #endregion
+    }
+}
