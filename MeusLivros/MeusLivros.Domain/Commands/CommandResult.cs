0ec8209ec7e32be9ef665699e5d449b1a9b49714
Marcos Dias Vendramini <marcosdiasvendramini@yahoo.com.br>
2024-10-22 22:09:57 -0300

    Aula11 - 22-10-2024

---
+++ MeusLivros/MeusLivros.Domain/Commands/CommandResult.cs
@@ -0,0 +1,20 @@
+﻿using MeusLivros.Domain.Commands.Interfaces;
+
+namespace MeusLivros.Domain.Commands
+{
+    public class CommandResult : ICommandResult
+    {
+        public CommandResult(bool sucesso, string mensagem, object dados)
+        {
+            Sucesso = sucesso;
+            Mensagem = mensagem;
+            Dados = dados;
+        }
+
+        public CommandResult() { }
+
+        public bool Sucesso { get; set; }
+        public string Mensagem { get; set; }
+        public object Dados { get; set; }
+    }
+}
