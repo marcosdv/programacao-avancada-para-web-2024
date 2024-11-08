0a9cd205c89331f393de48475b6d7a6dd544e151
Marcos Dias Vendramini <marcosdiasvendramini@yahoo.com.br>
2024-11-07 22:48:29 -0300

    Aula13 - 05-11-2024

--- MeusLivros/MeusLivros.Domain/Handlers/Interfaces/IHandler.cs
+++
@@ -1,9 +0,0 @@
-﻿using MeusLivros.Domain.Commands.Interfaces;
-
-namespace MeusLivros.Domain.Handlers.Interfaces
-{
-    public interface IHandler<T> where T : ICommand
-    {
-        ICommandResult Execute(T command);
-    }
-}
